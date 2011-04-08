7b5b06b813d434c3cea589d1b688330cd4cc3afe
Erik Doernenburg <erik@doernenburg.com>
2011-04-08 11:22:28 +0000

    First step towards Xcode 4 project structure.

---
+++ CCMenuTests/CCMenuTests.h
@@ -0,0 +1,17 @@
+//
+//  CCMenuTests.h
+//  CCMenuTests
+//
+//  Created by Erik Doernenburg on 8/04/11.
+//  Copyright 2011 ThoughtWorks. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface CCMenuTests : SenTestCase {
+@private
+    
+}
+
+@end
