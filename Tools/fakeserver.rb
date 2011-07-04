e7aa44344bcce2d952c6096bd1535aa73601ab97
Erik Doernenburg <erik@doernenburg.com>
2011-07-04 08:57:49 +0000

    Correct reporting of responses from server without project status; usually well-formed XHTML.

--- Tools/fakeserver.rb
+++ Tools/fakeserver.rb
@@ -11,9 +11,9 @@ get '/cctray.xml' do
 	body = 
 	'<Projects>
     <Project name="connectfour" activity="Sleeping" lastBuildStatus="Success" lastBuildLabel="build.1" 
-       lastBuildTime="2007-07-18T18:44:48" webUrl="http://localhost:8080/dashboard/build/detail/connectfour"/>
+       lastBuildTime="2007-07-18T18:44:48" webUrl="http://localhost:4567/dashboard/build/detail/connectfour"/>
   </Projects>'
-     
+
 	[200, {"Content-Type" => "application/xml"}, body]
 	 
 end
@@ -21,6 +21,14 @@ end
 get '/cctray.html' do
 
   body = '<html><body>sdhfsjkdf<p></body></html>'
+
+  [200, {"Content-Type" => "text/html"}, body]
+
+end
+                                             
+get '/dashboard/build/detail/connectfour' do
+
+  body = '<html><body><h1>Connect Four</h1>This is the project page on the build server.</body></html>'
 
   [200, {"Content-Type" => "text/html"}, body]
 
