2c10c0dcf02176cbb6ab687067681e0eb77fa42d
Erik Doernenburg <erik@doernenburg.com>
2011-07-22 13:17:04 +0000

    Improved fake server. Don't need to restart for status changes.

--- Tools/fakeserver.rb
+++ Tools/fakeserver.rb
@@ -8,28 +8,14 @@ end
 
 get '/cctray.xml' do
 
-	body = 
-	'<Projects>
-    <Project name="connectfour" activity="Sleeping" lastBuildStatus="Success" lastBuildLabel="build.1" 
-       lastBuildTime="2007-07-18T18:44:48" webUrl="http://localhost:4567/dashboard/build/detail/connectfour"/>
-  </Projects>'
-
+	body = File.open('fakeresponse.xml').read()
 	[200, {"Content-Type" => "application/xml"}, body]
 	 
 end
 
-get '/cctray.html' do
-
-  body = '<html><body>sdhfsjkdf<p></body></html>'
-
-  [200, {"Content-Type" => "text/html"}, body]
-
-end
-                                             
 get '/dashboard/build/detail/connectfour' do
 
   body = '<html><body><h1>Connect Four</h1>This is the project page on the build server.</body></html>'
-
   [200, {"Content-Type" => "text/html"}, body]
 
 end
