f3a89049a4c0aa95dc1258d7dd0e9485248aa957
Erik Doernenburg <erik@doernenburg.com>
2011-12-29 12:35:58 +0000

    Some gold-plating in fake server...

--- Tools/fakeserver.rb
+++ Tools/fakeserver.rb
@@ -3,13 +3,15 @@ require 'haml'
 
 $KCODE = 'u' if RUBY_VERSION < '1.9'
 
-@@ACTIVITY = "Sleeping"
-@@STATUS = "Success"
+@@ACTIVITY = :Sleeping
+@@STATUS = :Success
 @@BUILD_NUM = 1
 @@BUILD_TIME = "2007-07-18T18:44:48"
 
-before do
-	content_type :html, 'charset' => 'utf-8'
+helpers do
+  def is_building()
+    @@ACTIVITY != :Sleeping
+  end
 end
 
 get '/' do
@@ -21,32 +23,27 @@ get '/control' do
 end
 
 post '/control/build' do
-  @@ACTIVITY = 'Building'
+  @@ACTIVITY = :Building
   @@BUILD_NUM += 1
   @@BUILD_TIME = Time.now.iso8601
   redirect "/control"
 end
 
 post '/control/success' do
-  @@STATUS = 'Success'
-  @@ACTIVITY = 'Sleeping'
+  @@STATUS = :Success
+  @@ACTIVITY = :Sleeping
   redirect "/control"
 end
 
 post '/control/failure' do
-  @@STATUS = "Failure"
-  @@ACTIVITY = "Sleeping"
+  @@STATUS = :Failure
+  @@ACTIVITY = :Sleeping
   redirect "/control"
 end
 
 get '/cctray.xml' do
-  body = "
-  <Projects>
-    <Project name='connectfour' activity='#{@@ACTIVITY}' lastBuildStatus='#{@@STATUS}'
-             lastBuildLabel='build.#{@@BUILD_NUM}' lastBuildTime='#{@@BUILD_TIME}'
-             webUrl='http://localhost:4567/dashboard/build/detail/connectfour' />
-  </Projects>"
-	[200, {"Content-Type" => "application/xml"}, body]
+  content_type :xml
+  haml :cctray
 end
 
 get '/dashboard/build/detail/connectfour' do
@@ -56,39 +53,44 @@ end
 
 __END__
 
-@@ layout
+@@ control
 !!! 5
 %html
-  = yield
+  %h1 Fake CI Server
+  %table
+    %tr
+      %td Activitiy:	
+      %td= @@ACTIVITY
+    %tr
+      %td Status:
+      %td= @@STATUS
+    %tr
+      %td Build number:	
+      %td= @@BUILD_NUM
+    %tr
+      %td Build time:	
+      %td= @@BUILD_TIME
+  %p 
+  %form{:name => "input", :action => "control/build", :method => "post"}
+    %input{:type => "submit", :value => "Start build", :disabled => is_building() }
+  %form{:name => "input", :action => "control/success", :method => "post"}
+    %input{:type => "submit", :value => "Success", :disabled => !is_building() }
+  %form{:name => "input", :action => "control/failure", :method => "post"}
+    %input{:type => "submit", :value => "Failure", :disabled => !is_building() }
 
 
-@@ control
-%h1 Fake CI Server
-%table
-  %tr
-    %td Activitiy:	
-    %td= @@ACTIVITY
-  %tr
-    %td Status:
-    %td= @@STATUS
-  %tr
-    %td Build number:	
-    %td= @@BUILD_NUM
-  %tr
-    %td Build time:	
-    %td= @@BUILD_TIME
-%p 
-%form{:name => "input", :action => "control/build", :method => "post"}
-  %input{:type => "submit", :value => "Start build", :disabled => @@ACTIVITY != "Sleeping" }
-%form{:name => "input", :action => "control/success", :method => "post"}
-  %input{:type => "submit", :value => "Success", :disabled => @@ACTIVITY == "Sleeping" }
-%form{:name => "input", :action => "control/failure", :method => "post"}
-  %input{:type => "submit", :value => "Failure", :disabled => @@ACTIVITY == "Sleeping" }
+@@ cctray
+!!! XML
+%Projects
+  %Project{:name => 'connectfour', :webUrl => 'http://localhost:4567/dashboard/build/detail/connectfour',
+    :activity => @@ACTIVITY, :lastBuildStatus => @@STATUS,
+    :lastBuildLabel => "build.#{@@BUILD_NUM}", :lastBuildTime => @@BUILD_TIME}
 
 
 @@ project
-
-%h1 Connect Four
-%p This is the project page on the build server.
+!!! 5
+%html
+  %h1 Connect Four
+  %p This is the project page on the build server.
 
 
