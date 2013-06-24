3f2f27a060dbe98d943cc2c49a04bc92dd20eec2
Erik Doernenburg <erik@doernenburg.com>
2013-06-24 11:43:35 +0000

    Preparation for 1.6 releases.

--- CCMenu/Classes/CCMKeychainHelper.h
+++ CCMenu/Classes/CCMKeychainHelper.h
@@ -3,13 +3,14 @@
 
 @interface CCMKeychainHelper : NSObject
 
-+ (NSString *)accountForURLString:(NSString *)aString error:(NSError **)errorPtr;
-+ (NSString *)accountForURL:(NSURL *)aURL error:(NSError **)errorPtr;
++ (BOOL)setPassword:(NSString *)password forURLString:(NSString *)aString error:(NSError **)errorPtr;
++ (BOOL)setPassword:(NSString *)password forURL:(NSURL *)aURL error:(NSError **)errorPtr;
 
 + (NSString *)passwordForURLString:(NSString *)aString error:(NSError **)errorPtr;
 + (NSString *)passwordForURL:(NSURL *)aURL error:(NSError **)errorPtr;
 
-+ (BOOL)setPassword:(NSString *)password forURLString:(NSString *)aString error:(NSError **)errorPtr;
-+ (BOOL)setPassword:(NSString *)password forURL:(NSURL *)aURL error:(NSError **)errorPtr;
++ (NSString *)accountForURLString:(NSString *)aString error:(NSError **)errorPtr;
++ (NSString *)accountForURL:(NSURL *)aURL error:(NSError **)errorPtr;
+
 
 @end
--- CCMenu/Classes/CCMKeychainHelper.m
+++ CCMenu/Classes/CCMKeychainHelper.m
@@ -3,6 +3,73 @@
 
 @implementation CCMKeychainHelper
 
++ (BOOL)setPassword:(NSString *)password forURLString:(NSString *)aString error:(NSError **)errorPtr
+{
+    return [self setPassword:password forURL:[NSURL URLWithString:aString] error:errorPtr];
+}
+
++ (BOOL)setPassword:(NSString *)password forURL:(NSURL *)aURL error:(NSError **)errorPtr
+{
+    NSDictionary *query = @{
+        (id)kSecClass: (id)kSecClassInternetPassword,
+        (id)kSecAttrServer: [aURL host],
+        (id)kSecAttrPort: [aURL port],
+        (id)kSecAttrAccount: [aURL user]
+    };
+    NSDictionary *item = @{
+        (id)kSecClass: (id)kSecClassInternetPassword,
+        (id)kSecAttrServer: [aURL host],
+        (id)kSecAttrPort: [aURL port],
+        (id)kSecAttrAccount: [aURL user],
+        (id)kSecAttrProtocol: [aURL scheme],
+        (id)kSecValueData:[password dataUsingEncoding:NSUTF8StringEncoding]
+    };
+
+    OSStatus status = SecItemAdd((CFDictionaryRef)item, NULL);
+    if(status == errSecDuplicateItem)
+        status = SecItemUpdate((CFDictionaryRef)query, (CFDictionaryRef)item);
+
+    if(status != noErr)
+    {
+        if(errorPtr != NULL)
+            *errorPtr = [NSError errorWithDomain:NSOSStatusErrorDomain code:status userInfo:nil];
+        return NO;
+    }
+    return YES;
+}
+
+
++ (NSString *)passwordForURLString:(NSString *)aString error:(NSError **)errorPtr
+{
+    return [self passwordForURL:[NSURL URLWithString:aString] error:errorPtr];
+}
+
++ (NSString *)passwordForURL:(NSURL *)aURL error:(NSError **)errorPtr
+{
+    if([aURL user] == nil)
+        return nil;
+
+    NSDictionary *query = @{
+        (id)kSecClass: (id)kSecClassInternetPassword,
+        (id)kSecAttrServer: [aURL host],
+        (id)kSecAttrPort: [aURL port],
+        (id)kSecAttrAccount: [aURL user],
+        (id)kSecReturnData: @YES
+    };
+    NSData *data = NULL;
+
+    OSStatus status = SecItemCopyMatching((CFDictionaryRef)query, (CFTypeRef*)&data);
+
+    if(status != noErr)
+    {
+        if(errorPtr != NULL)
+            *errorPtr = [NSError errorWithDomain:NSOSStatusErrorDomain code:status userInfo:nil];
+        return nil;
+    }
+    return [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+}
+
+
 + (NSString *)accountForURLString:(NSString *)aString error:(NSError **)errorPtr
 {
     return [self accountForURL:[NSURL URLWithString:aString] error:errorPtr];
@@ -31,71 +98,5 @@
     return [attributes objectForKey:kSecAttrAccount];
 }
 
-
-+ (NSString *)passwordForURLString:(NSString *)aString error:(NSError **)errorPtr
-{
-    return [self passwordForURL:[NSURL URLWithString:aString] error:errorPtr];
-}
-
-+ (NSString *)passwordForURL:(NSURL *)aURL error:(NSError **)errorPtr
-{
-    if([aURL user] == nil)
-        return nil;
-
-    NSDictionary* query = @{
-        (id)kSecClass: (id)kSecClassInternetPassword,
-        (id)kSecAttrServer: [aURL host],
-        (id)kSecAttrPort: [aURL port],
-        (id)kSecAttrAccount: [aURL user],
-        (id)kSecReturnData: @YES
-    };
-    NSData *data = NULL;
-
-    OSStatus status = SecItemCopyMatching((CFDictionaryRef)query, (CFTypeRef*)&data);
-
-    if(status != noErr)
-    {
-        if(errorPtr != NULL)
-            *errorPtr = [NSError errorWithDomain:NSOSStatusErrorDomain code:status userInfo:nil];
-        return nil;
-    }
-    return [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
-}
-
-
-+ (BOOL)setPassword:(NSString *)password forURLString:(NSString *)aString error:(NSError **)errorPtr
-{
-    return [self setPassword:password forURL:[NSURL URLWithString:aString] error:errorPtr];
-}
-
-+ (BOOL)setPassword:(NSString *)password forURL:(NSURL *)aURL error:(NSError **)errorPtr
-{
-    NSDictionary * query = @{
-        (id)kSecClass: (id)kSecClassInternetPassword,
-        (id)kSecAttrServer: [aURL host],
-        (id)kSecAttrPort: [aURL port],
-        (id)kSecAttrAccount: [aURL user]
-    };
-    NSDictionary* item = @{
-        (id)kSecClass: (id)kSecClassInternetPassword,
-        (id)kSecAttrServer: [aURL host],
-        (id)kSecAttrPort: [aURL port],
-        (id)kSecAttrAccount: [aURL user],
-        (id)kSecAttrProtocol: [aURL scheme],
-        (id)kSecValueData:[password dataUsingEncoding:NSUTF8StringEncoding]
-    };
-
-    OSStatus status = SecItemAdd((CFDictionaryRef)item, NULL);
-    if(status == errSecDuplicateItem)
-        status = SecItemUpdate((CFDictionaryRef)query, (CFDictionaryRef)item);
-
-    if(status != noErr)
-    {
-        if(errorPtr != NULL)
-            *errorPtr = [NSError errorWithDomain:NSOSStatusErrorDomain code:status userInfo:nil];
-        return NO;
-    }
-    return YES;
-}
 
 @end
