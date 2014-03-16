bd8634a045b381768b347fb7f325c716b91bc637
Erik Doernenburg <erik@doernenburg.com>
2014-03-16 12:17:20 +0000

    Reverted to previous, simpler, update check in anticipation of App Store only releases.

--- CCMenu/Classes/CCMAboutPanelController.h
+++ CCMenu/Classes/CCMAboutPanelController.h
@@ -1,25 +1,13 @@
 
 #import <Cocoa/Cocoa.h>
-#import <Sparkle/Sparkle.h>
 
 
 @interface CCMAboutPanelController : NSObject
 {
-#ifndef CCM_MAS_BUILD
-	IBOutlet SUUpdater		*updater;
-#endif
     IBOutlet NSPanel        *aboutPanel;
     IBOutlet NSTextField	*versionField;
-#ifndef CCM_MAS_BUILD
-    IBOutlet NSTextField    *updateNoteField;
-    IBOutlet NSButton       *updateInstallButton;
-#endif
 }
 
 - (IBAction)showWindow:(id)sender;
-
-#ifndef CCM_MAS_BUILD
-- (IBAction)checkForUpdateNow:(id)sender;
-#endif
 
 @end
--- CCMenu/Classes/CCMAboutPanelController.m
+++ CCMenu/Classes/CCMAboutPanelController.m
@@ -20,32 +20,8 @@
 #endif
 		[versionField setStringValue:[NSString stringWithFormat:@"%@ (%@%@)", shortVersion, version, build]];
 	}
-#ifndef CCM_MAS_BUILD
-    [updateNoteField setStringValue:NSLocalizedString(@"Checking for update...", "For note in about panel")];
-    [updateInstallButton setHidden:YES];
-    [updater checkForUpdateInformation];
-#endif
 	[NSApp activateIgnoringOtherApps:YES];
 	[aboutPanel makeKeyAndOrderFront:self];
 }
-
-#ifndef CCM_MAS_BUILD
-- (IBAction)checkForUpdateNow:(id)sender
-{
-    [updater checkForUpdates:sender];
-}
-
-- (void)updater:(SUUpdater *)updater didFindValidUpdate:(SUAppcastItem *)update
-{
-    [updateInstallButton setHidden:NO];
-    [updateNoteField setStringValue:NSLocalizedString(@"An update is available", "For note in about panel")];
-}
-
-- (void)updaterDidNotFindUpdate:(SUUpdater *)update
-{
-    [updateNoteField setStringValue:NSLocalizedString(@"CCMenu is up to date", "For note in about panel")];
-}
-
-#endif
 
 @end
