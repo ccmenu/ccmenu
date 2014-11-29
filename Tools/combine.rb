3d473d37c6cc374d06b42648ecd274caea1a1e98
Erik Doernenburg <erik@doernenburg.com>
2014-11-29 20:44:55 +0000

    Added retina icons.

---
+++ Tools/combine.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+Dir.entries(".").each do | name |
+  next unless name =~ /^[\w-]+\.png/
+  cmd = "tiffutil -cathidpicheck #{name} #{name.gsub(/\./, "@2x.")} -out #{name.gsub(/png$/, "tiff")}"
+  puts "+ #{cmd}"
+  `#{cmd}`
+end
