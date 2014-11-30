79e4b992eef53dac3e92bbf98168eb046a194557
Erik Doernenburg <erik@doernenburg.com>
2014-11-30 22:28:29 +0000

    AppStore changes for 1.8 release.

--- CCMenu/Classes/CCMAppController.m
+++ CCMenu/Classes/CCMAppController.m
@@ -48,30 +48,6 @@
     [userNotificationHandler start];
 }
 
-- (void)showAppStoreReminder
-{
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-//    [defaults setBool:NO forKey:@"SupressAppStoreReminder"];
-    if([defaults boolForKey:@"SupressAppStoreReminder"])
-        return;
-        
-    NSAlert *alert = [[NSAlert alloc] init];
-    [alert setMessageText:@"Moving to the App Store"];
-    [alert setInformativeText:@"Starting with the next major version CCMenu will only be distributed via the App Store. If you do not switch to the App Store version you will not be notified of future updates. Please go to the App Store now and download CCMenu from there.\n\nCCMenu will remain open source software, and it will remain free. We are focusing on the App Store as a release channel in order to reduce the overhead when publishing new versions of CCMenu."];
-    [alert addButtonWithTitle:@"Go to App Store"];
-    [alert addButtonWithTitle:@"Cancel"];
-    [alert setShowsSuppressionButton:YES];
-
-    NSInteger result = [alert runModal];
-    if(result == NSAlertFirstButtonReturn)
-        [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"macappstores://itunes.apple.com/us/app/ccmenu/id603117688"]];
-    
-    if([[alert suppressionButton] state] == NSOnState)
-        [defaults setBool:YES forKey:@"SupressAppStoreReminder"];
-    
-    [alert release];
-}
-
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
 	@try
@@ -86,8 +62,6 @@
         NSUserNotification *userNotification = [[aNotification userInfo] objectForKey:@"NSApplicationLaunchUserNotificationKey"];
         if(userNotification != nil)
             [userNotificationHandler userNotificationCenter:nil didActivateNotification:userNotification];
-        else
-            [self showAppStoreReminder];
     }
 	@catch(NSException *e)
 	{
