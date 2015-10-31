ccmenu/ccmenu

commit d241f611faab26bb5305d1b2eecc0b40d72b4075

Made key equivalents work in menu.

CCMenu/Classes/CCMStatusItemApplication.m
@@ -13,14 +13,26 @@ static NSDictionary *bindings;
 
 - (void)sendEvent:(NSEvent *)event
 {
+    if(([NSApp mainMenu] != nil) && ([[NSApp mainMenu] performKeyEquivalent:event] == YES))
+    {
+        return;
+    }
+    
 	if(([event type] == NSKeyDown) && (![event isARepeat]) && ([event modifierFlags] & NSCommandKeyMask))
 	{
-		NSString *action = [bindings objectForKey:[event characters]];
+        [[NSApp mainMenu] performKeyEquivalent:event];
+        return;
+        
+        NSString *action = [bindings objectForKey:[event characters]];
 		if(action != nil)
+        {
 			[[[self keyWindow] firstResponder] doCommandBySelector:NSSelectorFromString(action)];
+        }
+        
 		return;
 	}
-	[super sendEvent:event];
+
+    [super sendEvent:event];
 }
 
 @end

commit 934cdde7f49c093e65cd6183e124202b2374791f

Fix for bug reported in #42.
A slightly embarrassing bug introduced in d241f611faab26bb5305d1b2eecc0b40d72b4075.

CCMenu/Classes/CCMStatusItemApplication.m
@@ -20,9 +20,6 @@ static NSDictionary *bindings;
     
 	if(([event type] == NSKeyDown) && (![event isARepeat]) && ([event modifierFlags] & NSCommandKeyMask))
 	{
-        [[NSApp mainMenu] performKeyEquivalent:event];
-        return;
-        
         NSString *action = [bindings objectForKey:[event characters]];
 		if(action != nil)
         {