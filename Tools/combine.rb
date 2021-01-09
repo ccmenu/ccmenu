3b4a4b5f487198169711b01f94dc713cd7900a8b
Erik Doernenburg <erik@doernenburg.com>
2021-01-09 21:03:35 +0100

    Added hardened runtime settings and fixed prefs toolbar.

--- Tools/combine.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-
-Dir.entries(".").each do | name |
-  next unless name =~ /^[\w-]+\.png/
-  cmd = "tiffutil -cathidpicheck #{name} #{name.gsub(/\./, "@2x.")} -out #{name.gsub(/png$/, "tiff")}"
-  puts "+ #{cmd}"
-  `#{cmd}`
-end
---
+++ Tools/fakeserver.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+bundle exec ruby fakeserver.rb
+
